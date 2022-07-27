cdb66b94a993a823d78e52d5a65c7c0681debfe3
Imaad Ghoor <102949510+ImaadG@users.noreply.github.com>
2022-07-27 03:05:13 -0700

    Animated buttons

--- game.js
+++ game.js
@@ -12,6 +12,7 @@ $(".btn").click(function(){
     var userChosenColour = $(this).attr("id");
     userclickedPattern.push(userChosenColour);
     playSound(userChosenColour);
+    animatePress(userChosenColour);
 });
 
 function nextSequence() {
@@ -22,17 +23,23 @@ function nextSequence() {
 
     $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
     playSound(randomChosenColour);
+    animatePress(randomChosenColour);
 }
 
 function playSound(name){
     var audio = new Audio("sounds/"+name+".mp3");
     audio.play();
 }
-    // $("#"+name).on("click",function(){
-    //     var audio=new Audio("sounds/"+name+".mp3")
-    //     audio.play();
-    // });
 
+function animatePress(currentColour){
+    $("#"+currentColour).addClass("pressed");
+    setTimeout(function(){
+        $("#"+currentColour).removeClass("pressed")
+    },100);
+
+    // ^ In a nutshell
+    // setTimeout( function(){ *jQuery Code* }, 100);
+}
 
 // $(".btn").on("click",function(){
 //     playSound($(this).attr("id"));
